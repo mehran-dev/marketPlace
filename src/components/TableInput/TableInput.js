93b44a832a87c3e923c662cc3013d4067cd4e536
mehran <m.saghebi.developer@gmail.com>
2020-10-05 01:13:51 +0330

    mordad fighting with material ui in youtube

--- src/components/TableInput/TableInput.js
+++ src/components/TableInput/TableInput.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import css from "./TableInput.module.css";
-//import useAutocomplete from "@material-ui/lab/useAutocomplete";
 
 import TextField from "@material-ui/core/TextField";
 import Autocomplete, {
@@ -22,33 +21,12 @@ function TableInput(props) {
   // React Hooks //
 
   const filter = createFilterOptions();
-  //const [classes, setClasses] = useState([css.Input, css.touched]);
-  //console.log("line 5 classes", classes);
-  // const classes = [css.Input];
   const [value, setValue] = useState("null");
-  //const [focuseVal, setFocuseVal] = useState(null);
-  //const [closeVal, setCloseVal] = useState(null);
   const [val2Store, setVal2Store] = useState(null);
 
   useState(() => {
-    //setValue(props.value)
     setVal2Store(props.value)
   });
-
-  /*   const {
-      getRootProps,
-      getInputLabelProps,
-      getInputProps,
-      getListboxProps,
-      getOptionProps, 
-      groupedOptions,
-    } = useAutocomplete({
-      id: props.id,
-      key: "frmcontrol",
-      options: ["Line 34 tableinput"],
-      getOptionLabel: (option) => option.title,
-    });
-   */
 
   return (
     <Autocomplete
@@ -69,10 +47,8 @@ function TableInput(props) {
       freeSolo
       onChange={(event, newValue) => {
         setVal2Store(newValue)
-        // console.log("newVal onchange",newValue);
         props.getValue(props.id, newValue)
-        //  console.log("newVal is set Now");
-        //console.log("val2store onchange ",val2Store);
+
 
         if (typeof newValue === 'string') {
           setValue({
@@ -86,11 +62,10 @@ function TableInput(props) {
         } else {
           setValue(newValue);
         }
-        //  setVal2Store(newValue);
         props.getValue(props.id, newValue)
       }}
+
       onInputChange={(event, newValue) => {
-        //  setMyValue(newValue);
         setVal2Store(newValue);
         if (typeof newValue === "string") {
           setValue({
@@ -112,64 +87,25 @@ function TableInput(props) {
 
 
       }}
-      getOptionSelected={(option, value) => {
-        //console.log("op,val",value);
 
-      }
 
-      }
-      onFocus={(e) => {
-        // setMyValue(e.target.value);
-        // setFocuseVal(e.target.value);
-      }}
+
+
       onClose={(e) => {
-
-        // setMyValue(e.target.value);
-
-        //console.log("onclose e ",e.target.value);
-        // console.log("var2store onclose ",val2Store);
-        // console.log("docgelID",document.getElementById(props.id));
 
         if (e.target.value !== 0) {
 
           props.getValue(props.id, val2Store)
         }
-
-
-
-      }
-      }
-      getOptionSelected={(option, value) => {
-        // console.log("OOOOP Fuckeer",option);
-        //return option === value
       }}
 
-      onKeyPress={(e) => {
 
-      }}
-      onKeyUp={(e) => {
-        //  console.log('kkkkkkkkkkkUUUUUUUp',e.target.value);
-        // props.getValue(props.id,e.target.value)
-      }}
       options={props.options ? props.options : []}
-      /*       getOptionLabel={(seggestions) => seggestions.title}
-       *//*  getOptionLabel={(option) => {
-// Value selected with enter, right from the input
-if (typeof option === "string") {
-return option;
-}
-// Add "xxx" option created dynamically
-if (option.inputValue) {
-return option.inputValue;
-}
-// Regular option
-return option.title;
-}} */
+
       getOptionLabel={(option) => {
-        // console.log('option',option)
-        // Value selected with enter, right from the input
+
         if (typeof option === 'string') {
-          //   console.log("option === 'string' is true");
+
           return option;
         }
         // Add "xxx" option created dynamically
@@ -199,8 +135,6 @@ return option.title;
         (options, params) => {
           //I changed the structure and params input val became null !! so I need to add existing val to it 
           params.inputValue = val2Store
-          //console.log('options',options);
-          //console.log('params',params);
           const filtered = filter(options, params);
 
 
